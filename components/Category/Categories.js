d3887f9b37dda3b75e310866f4987fc655d4b086
SaifKbishi <39298208+SaifKbishi@users.noreply.github.com>
2021-10-06 16:14:49 +0300

    imporving code from local code

--- components/Category/Categories.js
+++ components/Category/Categories.js
@@ -1,9 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Category from './Category';
-import { Portal, Provider } from 'react-native-paper';
+import { Portal, Provider, List,ListItemText } from 'react-native-paper';
 import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, TouchableOpacity,} from 'react-native';
-import DAL from '../../DAL/DAL'
 
 const categoriesURL = 'https://dev-agwa-public-static-assets-web.s3-us-west-2.amazonaws.com/data/catalogs/agwafarm.json';
 const plantsURL = 'https://dev-agwa-public-static-assets-web.s3-us-west-2.amazonaws.com/data/catalogs/plants.json';
@@ -11,39 +10,24 @@ let fullPlantData =[];
 const categoriesDataArray=[];
 
 const Categories = ()=>{
-  const [categoriesData, setCategoriesData] = useState([]);
-  const [plantsData, setPlantsData] = useState([]);
-  const [rawCategory, setRawCategory] = useState([]);
+  const [categoriesData, setCategoriesData] = useState([]);      
+  const [plantsData, setPlantsData] = useState([]); 
+  const [rawCategory, setRawCategory] = useState([]);   
  
   useEffect(() => {
-    let source = axios.CancelToken.source();
-    getCategories(); 
-    getPlants(); 
-    
-    addCategoryDetailsToPlantObj();   
-
+    let source = axios.CancelToken.source();         
+    getCategories();
+    getPlants();
     return () => { 
-      source.cancel('Cancelling in cleanup');
-    }   
+      source.cancel('Cancelling in cleanup');   
+    }    
   }, []);
  
   const getCategories = async () =>{  
     try{
       const cateResponse = await axios.get(categoriesURL)
-        setRawCategory(cateResponse.data.categories);
-        console.log(cateResponse.data.categories.length);
-        cateResponse.data.categories.forEach(cateItem => {
-        cateItem.plants.forEach(plant => {
-            let cateObj= {
-              categoryId: cateItem.id,
-              categoryName: cateItem.name,
-              plantId: plant.id,
-              plantName: plant.name,
-            }
-            categoriesDataArray.push(cateObj);
-          });          
-        });
-        setCategoriesData(categoriesDataArray);
+        setRawCategory(cateResponse.data.categories);    
+        setCategoriesData(cateResponse.data.categories);
     }catch(error){  
       console.log('\n error in getCategories: ',error);    
       }   
@@ -52,38 +36,21 @@ const Categories = ()=>{
   const getPlants = async () =>{ 
     try{
       const plantsResponse = await axios.get(plantsURL);
-      console.log(plantsResponse.data.plants.length);
-        let plantsDataArray = plantsResponse.data.plants.map(plantItem => {   
-          return{
-            plantId:plantItem.id,
-            plantName:plantItem.name,
-            plantImageId:plantItem.imageId,
-            plantSeedToCrop:plantItem.seedToCrop,
-            plantYield:plantItem.yield,
-            plantLifeCycle:plantItem.lifeCycle,                    
-            plantDescription:plantItem.description,                    
-            plantNutritionFactsPortionInfo:plantItem.nutritionFacts.portionInfo,
-            plantNutritionFacts:plantItem.nutritionFacts.items,
-          }          
-        });
-        setPlantsData(plantsDataArray);
+        setPlantsData(plantsResponse.data.plants);
     }catch(error){console.log('\n error fetching Plants', error)}
-  }//getPlants
-    
-  const addCategoryDetailsToPlantObj = async ()=>{
-    if(plantsData.length > 0 && categoriesData.length >0){
-      fullPlantData = await plantsData.map(plant =>{
-        let plantInCate = categoriesData.find(pIC => pIC.plantId === plant.plantId);
-        return plantInCate ? {...plant, ...plantInCate} : plant;
-      });
-    }    
-  }//addCategoryDetailsToPlantObj
+  }//getPlants    
 
   return(
-    <View style={styles.vegeItem}>
-        <Category categoryDetails={rawCategory} fullPlantData={fullPlantData} />
+    <View style={styles.vegeItem}> 
+      {categoriesData && categoriesData.map((category, index)=>{
+        return(
+          <List.AccordionGroup id={index} key={index}>
+              <Category categoryId={category.id} categoryTitle={category.name} categoryPlants={category.plants} plantsData={plantsData} />
+          </List.AccordionGroup>
+        )
+      })}
     </View>    
-  )
+  ) 
 }
 
 const styles = StyleSheet.create({
--- components/Category/Category.js
+++ components/Category/Category.js
@@ -5,28 +5,54 @@ import { Portal, Provider } from 'react-native-paper';
 import { StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native';
 import VegeItem from '../Vegetables/VegeItem';
 
-const Category = ({categoryDetails, fullPlantData})=>{
+const Category = ({categoryTitle, categoryId, categoryPlants, plantsData})=>{
+  // console.log('9 categoryPlants: ',categoryPlants)
+  // console.log('10 plantsData:',plantsData)
   const [expanded, setExpanded] = React.useState(true);
   const handlePress = () => setExpanded(!expanded);
-  console.log('fullPlantData: \n', fullPlantData); 
-  
-  return(    
+  // console.log('fullPlantData: \n', fullPlantData.length); 
+
+  return(
+    // console.log(categoryPlants[0].name),
+    // console.log(plantsData[0].name),
+    console.log( '18',
+      plantsData.filter((plant) => { 
+        return categoryPlants.find((cp)=>{
+          return cp.id === plant.id
+        })
+      })
+    ),
     <>
-      {categoryDetails && 
+      {/* {categoryDetails && 
         categoryDetails.map((cate)=>{
           return(
-            <List.AccordionGroup id={cate.id} style={styles.AccordionGroup} key={cate.id}>
+            // <List.AccordionGroup id={cate.id} style={styles.AccordionGroup} key={cate.id}>
             <List.Accordion title={cate.name} id={cate.id} expanded={expanded} onPress={handlePress}>
-              {fullPlantData && fullPlantData.filter(plantDtls=> 
-                plantDtls.categoryId === cate.id).map((plantDetails, index)=>{
+{fullPlantData && fullPlantData.filter(plantDtls=> plantDtls.categoryId === cate.id)
+                .map((plantDetails, index)=>{
                   return(   
                     <VegeItem category={cate.id} plantDetails5={plantDetails} key={index}/>
                   )
                 })}
             </List.Accordion>
-            </List.AccordionGroup>  
+            // </List.AccordionGroup>  
           );
-        })}
+        })} */}
+      <List.Accordion title={categoryTitle} id={categoryId} expanded={expanded} onPress={handlePress}>
+        {plantsData.filter((plant) => { 
+          return categoryPlants.find((catPlant)=>{
+            return catPlant.id ===plant.id
+          })
+          }).map((plant_details, index)=>{
+            return <Text>{plant_details.name}</Text>
+            // return(   
+            //   <VegeItem plant_details={plant_details} key={index}/>
+            // )
+          })
+        }
+        
+      </List.Accordion >
+
     </>
   )
 }
